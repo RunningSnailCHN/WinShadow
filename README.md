36e3d30a70aa2bdbc0b6bcec245d7602e0c74a06
RunningSnailCHN <1250212787@qq.com>
2019-10-13 11:29:33 +0800

    modify readme & add wait time between repeat operate

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # WinShadow
 使用python实现windows操作的重放
+##环境准备
+- Python3.6+
+- pynput 1.4.4
+- pyautogui 0.9.47
+## 使用方式
+  安装好环境后，将源码下载到本地，进入到源码目录，执行python win_shadow.py 会出现一个操作面板，点击相应的选项即可进行操作录制，操作的回放及退出操作。
+*目前支持的操作有*：
+
+- 鼠标操作： 点击，右键，滚轮，选中，拖拽
+- 键盘操作：字符输入，简单组合键
+## 实现方式
+### 鼠标键盘监听
+使用pynput,官网<https://pypi.org/project/pynput/>进行鼠标键盘的监听。
+
+### 操作回放
+使用pyautogui,源码地址<https://github.com/asweigart/pyautogui>进行鼠标键盘的回放。这里需要注意的是监听结果的操作类型名称有些是不一致的，需要转换
+## 参考文档
+- <https://pypi.org/project/pynput/>
+- <https://pyautogui.readthedocs.io/en/latest/>
+- <https://github.com/asweigart/pyautogui>
--- win_shadow.py
+++ win_shadow.py
@@ -43,11 +43,10 @@ class MyListener(threading.Thread):
         try:
             if not isinstance(key,keyboard.Key):
                 print('alphanumeric key {0} pressed'.format(key.char))
-                # print(dir(key))
-                f.writelines("keyboard,{0},{1},{2}\n".format(1,key.char,datetime.datetime.now().timestamp()))
+                f.writelines("keyboard,{0},{1},{2}\n".format(1,datetime.datetime.now().timestamp(),key.char))
             else:
                 print('special key {0} pressed'.format(key))
-                self.f.writelines("keyboard,{0},{1},{2}\n".format(0,key.name,datetime.datetime.now().timestamp()))
+                self.f.writelines("keyboard,{0},{1},{2}\n".format(0,datetime.datetime.now().timestamp(),key.name))
 
         except AttributeError:
             pass
@@ -58,10 +57,10 @@ class MyListener(threading.Thread):
             if not isinstance(key,keyboard.Key):
                 print('alphanumeric key {0} released'.format(key.char))
                 # print(dir(key))
-                f.writelines("keyboard,{0},{1},{2}\n".format(2,key.char,datetime.datetime.now().timestamp()))
+                f.writelines("keyboard,{0},{1},{2}\n".format(2,datetime.datetime.now().timestamp(),key.char))
             else:
                 print('special key {0} released'.format(key))
-                self.f.writelines("keyboard,{0},{1},{2}\n".format(3,key.name,datetime.datetime.now().timestamp()))
+                self.f.writelines("keyboard,{0},{1},{2}\n".format(3,datetime.datetime.now().timestamp(),key.name))
 
         except AttributeError:
             pass
@@ -72,89 +71,98 @@ class MyListener(threading.Thread):
 
     def on_click(self,x, y, button, pressed):
         print('{0} at {1}'.format( 'Pressed' if pressed else 'Released',(x, y)))
-        self.f.writelines("mouse,{0},{1},{2},{3}\n".format(button.name+('_P' if pressed else '_R'),x,y,datetime.datetime.now().timestamp()))
+        self.f.writelines("mouse,{0},{1},{2},{3}\n".format(button.name+('_P' if pressed else '_R'),datetime.datetime.now().timestamp(),x,y))
         self.f.flush()
 
 
     def on_scroll(self,x, y, dx, dy):
         print('Scrolled {0} at {1}'.format('down' if dy < 0 else 'up',(x, y)))
-        self.f.writelines("mouse,scroll,{0},{1},{2},{3},{4}\n".format(x,y,dx,dy,datetime.datetime.now().timestamp()))
+        self.f.writelines("mouse,scroll,{0},{1},{2},{3},{4}\n".format(datetime.datetime.now().timestamp(),x,y,dx,dy))
         self.f.flush()
 
-# m = Controller()-
-def repeatMouseOps(ops):
-    print(ops)
-    ops_arr = ops.strip().split(",")
-    name = ops_arr[1]
-    if name== 'left_P' or name == "right_P":
-        # p.click(int(ops_arr[2]),int(ops_arr[3]))
-        p.mouseDown(int(ops_arr[2]),int(ops_arr[3]), button=name[:-2])
-    elif name == "left_R" or name == 'right_R':
-        # p.rightClick(int(ops_arr[2]),int(ops_arr[3]))
-        p.mouseUp(int(ops_arr[2]),int(ops_arr[3]), button=name[:-2])
-    elif name == 'middle':
-        p.middleClick(int(ops_arr[2]),int(ops_arr[3]))
-    elif name == 'scroll':
-        p.moveTo(int(ops_arr[2]),int(ops_arr[3]))
-        p.scroll(int(ops_arr[5]) * 120)
-        # m.scroll(int(ops_arr[4]),int(ops_arr[5]))
-    else:
-        print("not known ops.....")
-
-def repeatKeyboardOps(key_record):
-    print(key_record)
-    arrs = key_record.split(",")
-    name = arrs[1]
-    char = arrs[2]
-    if name == '0' or name == '1':
-        # p.typewrite(message=char,interval=0.01)
-        p.keyDown(char)
-    elif name == '3' or name == '2':
-        p.keyUp(char)
-    else:
-        keys = char.split("$")
-        cmd = "p.hotkey('"+"','".join(keys)+"')"
-        exec(cmd)
-        # pass
-        # p.press(char)
-
-def doShadow(file_name):
-    time.sleep(2)
-    stack = []
-    stack2 = []
-    with open(file_name,'r') as f:
-        for i in f.readlines():
-            print(i)
-            stack.append(i.replace("_r","right").replace("_l","left"))
-    while len(stack) > 0:
-        i = stack.pop()
-        if i.strip().startswith('mouse'):
-            stack2.append(i)
+# m = Controller()
+class MyShadow():
+    pre_time = 0
+    def repeatMouseOps(self,ops):
+        print(ops)
+        ops_arr = ops.strip().split(",")
+        name = ops_arr[1]
+        self.wait(float(ops_arr[2]))
+        if name == 'left_P' or name == "right_P":
+            # p.click(int(ops_arr[2]),int(ops_arr[3]))
+            p.mouseDown(int(ops_arr[3]),int(ops_arr[4]), button=name[:-2])
+        elif name == "left_R" or name == 'right_R':
+            # p.rightClick(int(ops_arr[2]),int(ops_arr[3]))
+            p.mouseUp(int(ops_arr[3]),int(ops_arr[4]), button=name[:-2])
+        elif name == 'middle':
+            p.middleClick(int(ops_arr[3]),int(ops_arr[4]))
+        elif name == 'scroll':
+            p.moveTo(int(ops_arr[3]),int(ops_arr[4]))
+            p.scroll(int(ops_arr[6]) * 120)
+            # m.scroll(int(ops_arr[4]),int(ops_arr[5]))
         else:
-            arrs = i.split(",")
-            tp = arrs[1]
-            name = arrs[2]
+            print("not known ops.....")
 
-            if tp == '3':
-                hotKey = []
-                hotKey.append(arrs[2])
-                v = stack.pop()
-                arrs2 = v.split(",")
-                while arrs2[1] != "0" and name != arrs2[2]:
-                    if not arrs2[2] in hotKey:
-                        hotKey.append(arrs2[2])
+    def repeatKeyboardOps(self,key_record):
+        print(key_record)
+        arrs = key_record.split(",")
+        name = arrs[1]
+        char = arrs[3]
+        self.wait(float(ops_arr[2]))
+        if name == '0' or name == '1':
+            # p.typewrite(message=char,interval=0.01)
+            p.keyDown(char)
+        elif name == '3' or name == '2':
+            p.keyUp(char)
+        else:
+            keys = char.split("$")
+            cmd = "p.hotkey('"+"','".join(keys)+"')"
+            exec(cmd)
+            # pass
+            # p.press(char)
+    def wait(self,timestamp):
+        if self.pre_time > 0:
+            cost = timestamp - self.pre_time
+            if cost > 1:
+                time.sleep(cost)
+            self.pre_time = timestamp
+    def doShadow(self,file_name):
+        time.sleep(2)
+        stack = []
+        stack2 = []
+        with open(file_name,'r') as f:
+            for i in f.readlines():
+                print(i)
+                stack.append(i.replace("_r","right").replace("_l","left"))
+        while len(stack) > 0:
+            i = stack.pop()
+            if i.strip().startswith('mouse'):
+                stack2.append(i)
+            else:
+                arrs = i.split(",")
+                tp = arrs[1]
+                name = arrs[3]
+
+                if tp == '3':
+                    hotKey = []
+                    hotKey.append(arrs[3])
                     v = stack.pop()
                     arrs2 = v.split(",")
-                stack2.append("keyboard,{0},{1},{2}\n".format('hot',"$".join(hotKey),arrs[3]))
+                    while arrs2[1] != "0" and name != arrs2[3]:
+                        if not arrs2[2] in hotKey:
+                            hotKey.append(arrs2[3])
+                        v = stack.pop()
+                        arrs2 = v.split(",")
+                    stack2.append("keyboard,{0},{1},{2}\n".format('hot',"$".join(hotKey),arrs[2]))
 
-    while len(stack2) > 0:
-        i = stack2.pop()
+        while len(stack2) > 0:
+            i = stack2.pop()
 
-        if i.strip().startswith('mouse'):
-            repeatMouseOps(i.strip())
-            time.sleep(1)
-        else:
-            repeatKeyboardOps(i.strip())
+            if i.strip().startswith('mouse'):
+                self.repeatMouseOps(i.strip())
+                time.sleep(1)
+            else:
+                self.repeatKeyboardOps(i.strip())
 
 
 if __name__ == "__main__":
@@ -166,6 +174,7 @@ if __name__ == "__main__":
     pause_flag = True
     name = 'Record'
     listener = MyListener(f)
+
     while flag:
         ac = p.confirm('Enter option.', buttons=[name, 'Repeat', 'Quit'])
         if ac == "Record":
@@ -191,7 +200,10 @@ if __name__ == "__main__":
                     import easygui
                     new_file_name = easygui.fileopenbox()
                     #new_file_name = p.prompt('input  ')
-                doShadow(new_file_name)
+                if new_file_name is None or new_file_name== "":
+                    continue
+                shadow = MyShadow()
+                shadow.doShadow(new_file_name)
         else:
             print("stop...")
             flag = False
